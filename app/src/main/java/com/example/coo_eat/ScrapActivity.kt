4edce01d9cdafcee9a730b1f1fc54cd7f2d39120
gyeongseoMin <67200721+gyeongseoMin@users.noreply.github.com>
2021-11-13 18:07:43 +0900

    add scrap page

---
+++ app/src/main/java/com/example/coo_eat/ScrapActivity.kt
@@ -0,0 +1,11 @@
+package com.example.coo_eat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScrapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.scrap)
+    }
+}
